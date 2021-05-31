6850f89482d44b4898c6b3cd0485fda1564732c0
Adsel <marcin.radwan08@gmail.com>
2021-05-31 22:39:25 +0200

    feat: patient navbar

--- src/app/patient-side/patient-panel/patient-panel.module.ts
+++ src/app/patient-side/patient-panel/patient-panel.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientPanelHomeComponent } from './patient-panel-home/patient-panel-home.component';
+import { PatientPanelNavbarComponent } from './patient-panel-navbar/patient-panel-navbar.component';
 
 
 
 @NgModule({
     declarations: [
-        PatientPanelHomeComponent
+        PatientPanelHomeComponent,
+        PatientPanelNavbarComponent
     ],
     exports: [
         PatientPanelHomeComponent
