1581cf763fc53cbce381e06a8334b417f8316e19
Adsel <marcin.radwan08@gmail.com>
2021-05-31 23:11:29 +0200

    feat: patient home page cards

--- src/app/patient-side/patient-panel/patient-panel-home/patient-panel-home.component.ts
+++ src/app/patient-side/patient-panel/patient-panel-home/patient-panel-home.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-panel-home.component.scss']
 })
 export class PatientPanelHomeComponent implements OnInit {
-
+  cards = [
+    {
+      title: 'Register visit',
+      src: 'https://images.pexels.com/photos/5473182/pexels-photo-5473182.jpeg?cs=srgb&dl=pexels-ryutaro-tsukata-5473182.jpg&fm=jpg',
+      text: 'Register new visit online!',
+      button: { label: 'Go to Visits', icon: 'fas fa-calendar-alt', href: '#' }
+    }, {
+      title: 'Visits history',
+      src: '...',
+      text: 'Show visits history',
+      button: { label: 'Go to Visit History', icon: 'fas fa-calendar-alt', href: '#' }
+    }, {
+      title: 'Manage account',
+      src: '...',
+      text: 'Manage your account data!',
+      button: { label: 'Manage', icon: 'fas fa-user', href: '#' }
+    }, {
+      title: 'Check physiotherapists',
+      src: '...',
+      text: 'Check our physio list!',
+      button: { label: 'Go to Physio List', icon: 'fas fa-glasses', href: '#' }
+    }, {
+      title: 'Have questions?',
+      src: '...',
+      text: 'Ask question online!',
+      button: { label: 'Ask Question', icon: 'fas fa-question', href: '#' }
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/patient-side/patient-panel/patient-panel.module.ts
+++ src/app/patient-side/patient-panel/patient-panel.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientPanelHomeComponent } from './patient-panel-home/patient-panel-home.component';
 import { PatientPanelNavbarComponent } from './patient-panel-navbar/patient-panel-navbar.component';
+import { PatientPanelHomeCardComponent } from './patient-panel-home-card/patient-panel-home-card.component';
 
 
 
 @NgModule({
     declarations: [
         PatientPanelHomeComponent,
-        PatientPanelNavbarComponent
+        PatientPanelNavbarComponent,
+        PatientPanelHomeCardComponent
     ],
     exports: [
         PatientPanelHomeComponent
