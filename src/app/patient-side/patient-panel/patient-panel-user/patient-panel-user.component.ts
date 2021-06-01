24c7690da58948671a4daa19e0cc400feaee1c4e
Adsel <marcin.radwan08@gmail.com>
2021-06-01 20:21:08 +0200

    refactor: deleted unused imports

--- src/app/patient-side/patient-panel/patient-panel-user/patient-panel-user.component.ts
+++ src/app/patient-side/patient-panel/patient-panel-user/patient-panel-user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../../core/auth.service';
 import {PatientService} from '../../../core/patient.service';
 import {ToastrService} from 'ngx-toastr';
