794a924ea9f36f4c23110c34a01d53e5197f6b68
Adsel <marcin.radwan08@gmail.com>
2021-06-01 09:25:51 +0200

    style: patient home view

--- src/app/patient-side/patient-panel/patient-panel-home/patient-panel-home.component.ts
+++ src/app/patient-side/patient-panel/patient-panel-home/patient-panel-home.component.ts
@@ -14,22 +14,22 @@ export class PatientPanelHomeComponent implements OnInit {
       button: { label: 'Go to Visits', icon: 'fas fa-calendar-alt', href: '#' }
     }, {
       title: 'Visits history',
-      src: '...',
+      src: 'https://images.pexels.com/photos/4506109/pexels-photo-4506109.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4506109.jpg&fm=jpg',
       text: 'Show visits history',
       button: { label: 'Go to Visit History', icon: 'fas fa-calendar-alt', href: '#' }
     }, {
       title: 'Manage account',
-      src: '...',
+      src: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       text: 'Manage your account data!',
       button: { label: 'Manage', icon: 'fas fa-user', href: '#' }
     }, {
       title: 'Check physiotherapists',
-      src: '...',
+      src: 'https://cdn.pixabay.com/photo/2021/05/29/01/55/physical-therapy-6292317_960_720.jpg',
       text: 'Check our physio list!',
       button: { label: 'Go to Physio List', icon: 'fas fa-glasses', href: '#' }
     }, {
       title: 'Have questions?',
-      src: '...',
+      src: 'https://cdn.pixabay.com/photo/2015/03/24/12/42/woman-687560_960_720.jpg',
       text: 'Ask question online!',
       button: { label: 'Ask Question', icon: 'fas fa-question', href: '#' }
     }
